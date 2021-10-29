f97f5a169fc86b6cd224dade837dc331aec511ea
Rahjael <surfer.vortex@gmail.com>
2021-10-29 12:39:51 +0200

    test

---
+++ index.js
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+
+
+
+function getSizes() {
+  return {width: window.innerWidth, height: window.innerHeight}
+}
+
+function printSizes() {  
+  const h1 = document.querySelector('#label');
+  const sizes = getSizes();
+  h1.innerText = sizes.width + "x" + sizes.height;
+}
+
+
+window.addEventListener('orientationchange', () => {
+  printSizes();
+})
