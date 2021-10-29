aa1374d220c8342a8620647e15dfea98a8fd9c4c
Rahjael <surfer.vortex@gmail.com>
2021-10-29 16:58:46 +0200

    test

--- index.js
+++ index.js
@@ -2,17 +2,28 @@ function getSizes() {
   return {width: window.innerWidth, height: window.innerHeight}
 }
 
-function printSizes() {  
+function printSizes() {
   const h1 = document.querySelector('#label');
   const sizes = getSizes();
   h1.innerText = sizes.width + "x" + sizes.height + " " + screen.orientation.angle;
 }
 
 
-screen.orientation.addEventListener('change', () => {
+screen.orientation.addEventListener('change', (event) => {
   printSizes();
+  document.querySelector('#angle').innerText = event;
 });
 
+
+
+
+// const accelerometer = new Accelerometer({frequency: 10});
+// accelerometer.addEventListener('reading', () => {
+//   const h1 = document.querySelector('#angle');
+//   const sizes = getSizes();
+//   h1.innerText = ;
+
+// })
 
 printSizes();
 
