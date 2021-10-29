a9c632281aeebd524b61878bcda1d84bb6230ad4
Rahjael <surfer.vortex@gmail.com>
2021-10-29 16:19:58 +0200

    test

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function getSizes() {
 function printSizes() {  
   const h1 = document.querySelector('#label');
   const sizes = getSizes();
-  h1.innerText = sizes.width + "x" + sizes.height;
+  h1.innerText = sizes.width + "x" + sizes.height + " " + screen.orientation;
 }
 
 
