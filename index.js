8043be6f7bd0850836f37e01bdcd8e1b28703b17
Rahjael <surfer.vortex@gmail.com>
2021-10-30 11:40:32 +0200

    test

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ function stopSliding(e) {
 function slide(e) {
   bluSquare.style.transform = `translate(${e.clientX - bluSquare.offsetLeft}px, ${e.clientY}px)`;
 
+  document.querySelector('#label').innerText = `${e.clientX} x ${e.clientY}`;
+
   console.log("pointer: ", e.clientX, e.clientY);
   console.log("square: ", bluSquare.style.offsetX, bluSquare.style.bottom);
 }
@@ -43,35 +45,35 @@ console.log(bluSquare);
 
 
 
-function getSizes() {
-  return {width: window.innerWidth, height: window.innerHeight}
-}
+// function getSizes() {
+//   return {width: window.innerWidth, height: window.innerHeight}
+// }
 
-function printSizes() {
-  const h1 = document.querySelector('#label');
-  const sizes = getSizes();
-  h1.innerText = sizes.width + "x" + sizes.height + " " + screen.orientation.angle;
-}
-
-
-screen.orientation.addEventListener('change', (event) => {
-  printSizes();
-  document.querySelector('#angle').innerText = "gamma rotation: " + event.gamma;
-  document.querySelector('#third').innerText = "event";
-});
-
-
-
-
-// const accelerometer = new Accelerometer({frequency: 10});
-// accelerometer.addEventListener('reading', () => {
-//   const h1 = document.querySelector('#angle');
+// function printSizes() {
+//   const h1 = document.querySelector('#label');
 //   const sizes = getSizes();
-//   h1.innerText = ;
+//   h1.innerText = sizes.width + "x" + sizes.height + " " + screen.orientation.angle;
+// }
 
-// })
 
-printSizes();
+// screen.orientation.addEventListener('change', (event) => {
+//   printSizes();
+//   document.querySelector('#angle').innerText = "gamma rotation: " + event.gamma;
+//   document.querySelector('#third').innerText = "event";
+// });
+
+
+
+
+// // const accelerometer = new Accelerometer({frequency: 10});
+// // accelerometer.addEventListener('reading', () => {
+// //   const h1 = document.querySelector('#angle');
+// //   const sizes = getSizes();
+// //   h1.innerText = ;
+
+// // })
+
+// printSizes();
 
 
 
