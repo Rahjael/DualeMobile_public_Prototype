ff45ebd8f0ecf69c828172cc4350a2986f2b348c
Rahjael <surfer.vortex@gmail.com>
2021-10-30 10:45:09 +0200

    test

--- index.js
+++ index.js
@@ -1,3 +1,153 @@
+
+
+const bluSquare = document.querySelector('#square');
+
+
+
+if(window.PointerEvent) {
+  // Add Pointer Event Listener
+  bluSquare.addEventListener('pointerdown', this.handleGestureStart, true);
+  bluSquare.addEventListener('pointermove', this.handleGestureMove, true);
+  bluSquare.addEventListener('pointerup', this.handleGestureEnd, true);
+  bluSquare.addEventListener('pointercancel', this.handleGestureEnd, true);
+} else {
+  // Add Touch Listener
+  bluSquare.addEventListener('touchstart', this.handleGestureStart, true);
+  bluSquare.addEventListener('touchmove', this.handleGestureMove, true);
+  bluSquare.addEventListener('touchend', this.handleGestureEnd, true);
+  bluSquare.addEventListener('touchcancel', this.handleGestureEnd, true);
+
+  // Add Mouse Listener
+  bluSquare.addEventListener('mousedown', this.handleGestureStart, true);
+}
+
+
+// Handle the start of gestures
+this.handleGestureStart = function(event) {
+  event.preventDefault();
+
+  if(event.touches && event.touches.length > 1) {
+    return;
+  }
+
+  // Add the move and end listeners
+  if (window.PointerEvent) {
+    event.target.setPointerCapture(event.pointerId);
+  } else {
+    // Add Mouse Listeners
+    document.addEventListener('mousemove', this.handleGestureMove, true);
+    document.addEventListener('mouseup', this.handleGestureEnd, true);
+  }
+
+  initialTouchPos = getGesturePointFromEvent(event);
+
+  bluSquare.style.transition = 'initial';
+}.bind(this);
+
+
+// Handle end gestures
+this.handleGestureEnd = function(event) {
+  event.preventDefault();
+
+  if(event.touches && event.touches.length > 0) {
+    return;
+  }
+
+  rafPending = false;
+
+  // Remove Event Listeners
+  if (window.PointerEvent) {
+    event.target.releasePointerCapture(event.pointerId);
+  } else {
+    // Remove Mouse Listeners
+    document.removeEventListener('mousemove', this.handleGestureMove, true);
+    document.removeEventListener('mouseup', this.handleGestureEnd, true);
+  }
+
+  updateSwipeRestPosition();
+
+  initialTouchPos = null;
+}.bind(this);
+
+
+function getGesturePointFromEvent(event) {
+  var point = {};
+
+  if(event.targetTouches) {
+    // Prefer Touch Events
+    point.x = event.targetTouches[0].clientX;
+    point.y = event.targetTouches[0].clientY;
+  } else {
+    // Either Mouse event or Pointer Event
+    point.x = event.clientX;
+    point.y = event.clientY;
+  }
+
+  return point;
+}
+
+
+this.handleGestureMove = function (event) {
+  event.preventDefault();
+
+  if(!initialTouchPos) {
+    return;
+  }
+
+  lastTouchPos = getGesturePointFromEvent(event);
+
+  if(rafPending) {
+    return;
+  }
+
+  rafPending = true;
+
+  window.requestAnimFrame(onAnimFrame);
+}.bind(this);
+
+
+function onAnimFrame() {
+  if(!rafPending) {
+    return;
+  }
+
+  var differenceInX = initialTouchPos.x - lastTouchPos.x;
+
+  var newXTransform = (currentXPosition - differenceInX)+'px';
+  var transformStyle = 'translateX('+newXTransform+')';
+  bluSquare.style.webkitTransform = transformStyle;
+  bluSquare.style.MozTransform = transformStyle;
+  bluSquare.style.msTransform = transformStyle;
+  bluSquare.style.transform = transformStyle;
+
+  rafPending = false;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function getSizes() {
   return {width: window.innerWidth, height: window.innerHeight}
 }
