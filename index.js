ef4a66ddbf2bb79af2682891341fba1e5130f207
Rahjael <surfer.vortex@gmail.com>
2021-10-31 18:34:46 +0100

    test

--- index.js
+++ index.js
@@ -1,39 +1,126 @@
 
 
+let lastKnownScrollPosition = 0;
+let ticking = false;
+let currentlyHighlighted;
 
+// function doSomething(scrollPos) {
+//   // Do something with the scroll position
+// }
 
+function highlightCenterImage() {
+  let centerPoint = {
+    x: window.innerWidth / 2,
+    y: window.innerHeight / 2
+  }
 
-function beginMoving(e) {
-  bluSquare.onpointermove = moveSquare;
-  bluSquare.setPointerCapture(e.pointerId);
+  // Look for center row
+  let rows = Array.from(document.querySelectorAll('.photo-grid-row'));
+  // console.log(rows);
+  let targetRow = rows.find( row => {
+    let boundingRect = row.getBoundingClientRect();
+    return boundingRect.top < centerPoint.y && boundingRect.bottom > centerPoint.y;
+  });
+  // console.log(targetRow);
+  // If no row is selected, return
+  if(!targetRow) return;
+
+  // Look for center image
+  let images = Array.from(targetRow.querySelectorAll('.photo-grid-img'));
+  // console.log(images);
+  let targetImage = images.find( image => {
+    let boundingRect = image.getBoundingClientRect();
+    return boundingRect.top < centerPoint.y
+      && boundingRect.bottom > centerPoint.y
+      && boundingRect.left < centerPoint.x
+      && boundingRect.right > centerPoint.x;
+  });
+  // console.log(targetImage);
+  // If no image is selected
+  if(!targetImage) {
+    if(currentlyHighlighted)  {
+      currentlyHighlighted.classList.remove('photo-grid-img-highlighted');
+    }
+    return;
+  }
+
+  // If image is different from previous
+  if(targetImage != currentlyHighlighted) {
+    if(currentlyHighlighted)  {
+      currentlyHighlighted.classList.remove('photo-grid-img-highlighted');
+    }
+    targetImage.classList.add('photo-grid-img-highlighted');
+    currentlyHighlighted = targetImage;
+  }
 }
 
-function stopSliding(e) {
-  bluSquare.onpointermove = null;
-  bluSquare.releasePointerCapture(e.pointerId);
-}
+// https://developer.mozilla.org/en-US/docs/Web/API/Document/scroll_event
+document.addEventListener('scroll', function(e) {
+  // console.log("Scrolling");
+  lastKnownScrollPosition = window.scrollY;
 
-function moveSquare(e) {
-  bluSquare.style.transform = `translate(${e.clientX - bluSquare.offsetLeft}px, ${e.clientY}px)`;
+  if (!ticking) {
+    window.requestAnimationFrame(function() {
+      // doSomething(lastKnownScrollPosition);
+      highlightCenterImage();
+      ticking = false;
+    });
 
-  document.querySelector('#label').innerText = `${e.clientX} x ${e.clientY}`;
-
-  console.log("pointer: ", e.clientX, e.clientY);
-  console.log("square: ", bluSquare.style.offsetX, bluSquare.style.bottom);
-}
-
-
-const bluSquare = document.querySelector('#square');
-
-bluSquare.onpointerdown = beginSliding;
-bluSquare.onpointerup = stopSliding;
-
-console.log(bluSquare);
+    ticking = true;
+  }
+});
 
 
 
 
-document.querySelector('#angle').innerText = window.PointerEvent;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function beginMoving(e) {
+//   bluSquare.onpointermove = moveSquare;
+//   bluSquare.setPointerCapture(e.pointerId);
+// }
+
+// function stopSliding(e) {
+//   bluSquare.onpointermove = null;
+//   bluSquare.releasePointerCapture(e.pointerId);
+// }
+
+// function moveSquare(e) {
+//   bluSquare.style.transform = `translate(${e.clientX - bluSquare.offsetLeft}px, ${e.clientY}px)`;
+
+//   document.querySelector('#label').innerText = `${e.clientX} x ${e.clientY}`;
+
+//   console.log("pointer: ", e.clientX, e.clientY);
+//   console.log("square: ", bluSquare.style.offsetX, bluSquare.style.bottom);
+// }
+
+
+// const bluSquare = document.querySelector('#square');
+
+// bluSquare.onpointerdown = beginSliding;
+// bluSquare.onpointerup = stopSliding;
+
+// console.log(bluSquare);
+
+
+
+
+// document.querySelector('#angle').innerText = window.PointerEvent;
 
 
 
